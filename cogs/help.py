21d935fadaabca6930f86e55417e5ec3e9742ca2
Utkarsh Narain <utkarshnarain007@gmail.com>
2021-04-10 12:28:00 +0530

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -11,7 +11,7 @@ class Help(commands.Cog, name="Help command"):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"{self.class.name} Cog has been loaded. ")
+        print(f" Help Cog has been loaded. ")
 
     # @commands.command(name="help", description="This is the help command", usage="[slowmode time 1m, 1s 1h max 6h]", aliases=['h', 'sm'])
     # async def help(self,ctx,cog="1"):
