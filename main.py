739c4b1b02bf54587029bd6afb967e8d9f461f3c
Utkarsh Narain <utkarshnarain007@gmail.com>
2021-04-14 02:14:58 +0530

    local tokens to global

--- main.py
+++ main.py
@@ -23,11 +23,14 @@ client = commands.Bot(
 if os.path.exists(os.getcwd()+"./properties/tokens.json"):
     with open("./properties/tokens.json") as f:
         configData = json.load(f)
-else: 
-    configData = {
-        "token": "",
-        "mongo": ""
+else:
+    configTemplate = {
+            "token": "",
+            "mongo": ""
     }
+
+    with open(os.getcwd()+"./properties/tokens.json", "w+") as f:
+        json.dump(configTemplate, f)
 client.botToken = configData["token"]
 client.connection_url = configData["mongo"]
 
@@ -81,5 +84,5 @@ async def ping(ctx):
     """Bot Is dead"""
     await ctx.send(f'Pong! {round(client.latency*1000)}ms')
 
-# client.run(os.environ['BOT_TOKEN'])
-client.run(client.botToken)
+client.run(os.environ['BOT_TOKEN'])
+# client.run(client.botToken)
