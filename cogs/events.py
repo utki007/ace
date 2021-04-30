28214cb23667431745fc1f1033db1322a839fda8
Jay Patel <pateljay24004@gmail.com>
2021-04-30 14:41:22 +0530

    added Erro handler

---
+++ cogs/events.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+class Events(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+	    print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+
+	@commands.Cog.listener()
+	async def on_command_error(self, ctx. error):
+		ignored = (commands.CommandNotFound, commands.UserInputError)
+		if isinstance(error, ignored):
+		    return
+
+	    if isinstance(error, commands.CommandOnCooldown):
+	        # If the command is currently on cooldown trip this
+	        m, s = divmod(error.retry_after, 60)
+	        h, m = divmod(m, 60)
+	        if int(h) == 0 and int(m) == 0:
+	            await ctx.send(f" You must wait {int(s)} seconds to use this command!")
+	        elif int(h) == 0 and int(m) != 0:
+	            await ctx.send(
+	                f" You must wait {int(m)} minutes and {int(s)} seconds to use this command!"
+	            )
+	        else:
+	            await ctx.send(
+	                f" You must wait {int(h)} hours, {int(m)} minutes and {int(s)} seconds to use this command!"
+	            )
+        elif isinstance(error, commands.CheckFailure):
+            # If the command has failed a check, trip this
+            await ctx.send("Hey! You lack permission to use this command.")
+
+        else:
+        	raise error
+def setup(bot):
+    bot.add_cog(Events(bot)) 
