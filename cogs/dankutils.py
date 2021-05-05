0a245cf2b04195fa892553bccf0f505198f28eac
Utkarsh Narain <utkarshnarain007@gmail.com>
2021-05-05 23:19:46 +0530

    Added calculator and tax calculator

--- cogs/dankutils.py
+++ cogs/dankutils.py
@@ -10,11 +10,19 @@ import time
 import asyncio
 import math
 import datetime
+import TagScriptEngine
+from TagScriptEngine import Interpreter, adapter, block
 
 class dankutils(commands.Cog, name="Dank Utility"):
     
     def __init__(self, client):
         self.client = client
+        blocks = [
+            block.MathBlock(),
+            block.RandomBlock(),
+            block.RangeBlock(),
+        ]
+        self.engine = Interpreter(blocks)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -34,6 +42,39 @@ class dankutils(commands.Cog, name="Dank Utility"):
                         time.sleep(3)
                         return await message.channel.send("If you'd like to stop receiving pings, check out <#785882615202316298> for `@〖 🔕。No Partnership 〗` role!!!")
 
+    async def convert_to_numeral(self,query):
+        query = query.lower()
+        query = query.replace("k","000",100)
+        query = query.replace("m","000000",100)       
+        query = query.replace("b","000000000",100)  
+        return query
+    
+    @commands.command(aliases=["calc","c"])
+    async def calculate(self, ctx, *, query):
+        """Math"""
+        query = await self.convert_to_numeral(query)
+        query = query.replace(",", "")
+        engine_input = "{m:" + query + "}"
+        start = time.time()
+        output = self.engine.process(engine_input)
+        end = time.time()
 
+        output_string = output.body.replace("{m:", "").replace("}", "")
+        e = discord.Embed(
+            color= 0x9e3bff,
+            title=f"Calculated: `{int(float(output_string)):,}`",
+            description=f"Raw: `{int(float(output_string))}`",
+            timestamp=datetime.datetime.utcnow()
+        )
+        # e.set_footer(text=f"Calculated in {round((end - start) * 1000, 3)} ms")
+        await ctx.send(embed=e)
+    
+    @commands.command(aliases=["tc","taxc"])
+    async def taxcalculate(self,ctx, number: float):
+        """Finding tax"""
+        number = int(float(number))
+        await ctx.send(math.ceil((number*100)/92))
+    
+    
 def setup(client):
     client.add_cog(dankutils(client))
