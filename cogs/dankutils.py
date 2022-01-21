4ff8c1a289a38a9968e231e5eaf5257ed6664a05
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-01-21 11:27:09 +0530

    Update dankutils.py

--- cogs/dankutils.py
+++ cogs/dankutils.py
@@ -61,7 +61,8 @@ class dankutils(commands.Cog, description="Dank Utility"):
                             return msg.author.id == 810041263452848179
                         await partnerHeists.purge(limit=10, check=check, before=None)
                         return await message.channel.send("If you'd like to stop receiving pings, check out <#848631258137362452> for <@&810593886720098304> role!!! \n"
-                        f"||However, having <@&806795854475165736> makes <@&810593886720098304> useless, so remove <@&806795854475165736> role.||", allowed_mentions=am)
+                        f"||However, having <@&806795854475165736> makes <@&810593886720098304> useless, so remove <@&806795854475165736> role.||"
+                        f"**150 Mil Heist** <t:1642761032:R> <#927241961038045236> \nL2L for **Nitro** unlocks in <t:1642759230:R> <#933363028475396106>", allowed_mentions=am)
 
         # if message.channel.id == 840215557097390131 and message.author.id != self.bot.user.id:838646783785697290
         # 838646783785697290:
