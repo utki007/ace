53760aae7a4c0d7105ae7b1d47961245a96d4a77
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-03-01 06:22:02 +0530

    Made blacklisted partner cog user friendly

--- cogs/blacklist.py
+++ cogs/blacklist.py
@@ -15,146 +15,225 @@ from utils.Checks import checks
 
 class blacklist(commands.Cog, name="Blacklist", description="Blacklist a Partner or a server"):
 
-    def __init__(self,bot):
-        self.bot= bot
-        self.mongoconnection = self.bot.connection_url
-        self.mybot = pymongo.MongoClient(self.mongoconnection)
-        self.mydb = self.mybot['TGK']
-        self.mycol = self.mydb["blacklistServers"]
-        # for tgk
-        self.logChannel = int(858233010860326962)
+	def __init__(self,bot):
+		self.bot= bot
+		self.mongoconnection = self.bot.connection_url
+		self.mybot = pymongo.MongoClient(self.mongoconnection)
+		self.mydb = self.mybot['TGK']
+		self.mycol = self.mydb["blacklistServers"]
+		# for tgk
+		self.logChannel = int(858233010860326962)
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(f"{self.__class__.__name__} Cog has been loaded\n-----")
 
-    # add a server if it doesn't exist
-    async def create_blacklist(self, ctx, serverId, serverName, reason):
-        dict = {}
-        dict["_id"] = serverId
-        dict["serverName"] = serverName
-        dict["reason"] = reason
-        self.mycol.insert_one(dict)
+	# add a server if it doesn't exist
+	async def create_blacklist(self, ctx, serverId, serverName, reason, proof):
+		dict = {}
+		dict["_id"] = serverId
+		dict["serverName"] = serverName
+		dict["reason"] = reason
+		if proof == "":
+			proof = 'N/A'
+		dict["proof"] = proof
+		self.mycol.insert_one(dict)
 
-    @commands.group(name="Blacklist", aliases=['bl'])
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def blacklist(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await ctx.send(f"use `help blacklist` to know more!!!")
+	@commands.group(name="Blacklist", aliases=['bl'])
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def blacklist(self, ctx):
+		if ctx.invoked_subcommand is None:
+			await ctx.send(f"use `help blacklist` to know more!!!")
 
-    @blacklist.command(name="update", description="Add/Update Blacklisted Servers", aliases=['u'])
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def update(self, ctx, serverId: int, serverName: str, *reason):
-        reason = ' '.join([str(elem) for elem in reason])
-        myquery = {"_id": serverId}
-        info = self.mycol.find(myquery)
-        flag = 0
-        dict = {}
-        for x in info:
-            dict = x
-            flag = 1
+	@blacklist.command(name="update", description="Add/Update Blacklisted Servers", aliases=['u','add','edit'])
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def update(self, ctx, serverId: int, serverName: str):
+		# reason = ' '.join([str(elem) for elem in reason])
+		myquery = {"_id": serverId}
+		info = self.mycol.find(myquery)
+		flag = 0
+		dict = {}
+		for x in info:
+			dict = x
+			flag = 1
+		
+		await ctx.send(f"What is the reason for blacklisting **{serverName}**? ")
+		reason = None
+		try:
+			msg = await self.bot.wait_for("message", check=lambda m: m.author.id == ctx.author.id, timeout=600)
+			reason = msg.content.title()
+		except:
+			await ctx.send(f"{ctx.author.mention}, Blacklisting cancelled because there was no confirmation!")
+			return
 
-        if flag == 0:
-            try:
-                await self.create_blacklist(ctx, serverId, serverName, reason)
-                embed = discord.Embed(
-                    color=self.bot.colors["Success"],
-                    description=f'{self.bot.emojis_list["SuccessTick"]} | **{serverName.title()}** is successfully blacklisted!! ')
-                await ctx.channel.send(embed=embed)
-                return
-            except:
-                embed = discord.Embed(
-                    color=self.bot.colors["RED"],
-                    description=f'{self.bot.emojis_list["Warrning"]} | Unable to blacklist them. Contact Jay or utki.')
-                await ctx.channel.send(embed=embed)
-                return
+		await ctx.send(f"Proof for blacklisting **{serverName}**? `None for no evidence`" )
+		proof = None
+		proof_msg = ''
+		try:
+			msg = await self.bot.wait_for("message", check=lambda m: m.author.id == ctx.author.id, timeout=600)
+			if msg.content.lower() not in ["none"]:
+				proof = msg.content
+				prooflist = proof.split(' ')
+				counter = 1
+				for proof_index in range(len(prooflist)):
+					proof_msg += f' [Proof{counter}]({prooflist[proof_index]}) '
+					counter += 1
 
-        try:
-            newvalues = {"$set": {"serverName": serverName, "reason": reason}}
-            self.mycol.update_one(myquery, newvalues)
-            embed = discord.Embed(
-                    color=self.bot.colors["Success"],
-                    title=f'{self.bot.emojis_list["SuccessTick"]} | **{serverName.title()}** Blacklist reason updated successfully!! ',
-                    description=f"**Server ID:** {serverId}\n"
-                                f"**Server Name:** _{serverName.title()}_\n"
-                                f"**Reason:** _{reason}_ \n")
-            await ctx.channel.send(embed=embed)
-        except:
-            embed = discord.Embed(
-                    color=self.bot.colors["RED"],
-                    description=f'{self.bot.emojis_list["BrokenStatus"]} | Unable to update them. Contact Jay or utki.')
-            await ctx.channel.send(embed=embed)
-            return
+		except:
+			await ctx.send(f"{ctx.author.mention}, Blacklisting cancelled because there was no confirmation!")
+			return
 
-    @blacklist.command(name="information", description="Check if a server is blacklisted", aliases=['info'])
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def info(self, ctx, serverId: int):
-        
-        await ctx.message.delete()
-        myquery = {"_id": serverId}
-        info = self.mycol.find(myquery)
-        flag = 0
-        dict = {}
-        for x in info:
-            dict = x
-            flag = 1
+		if flag == 0:
+			try:
+				await self.create_blacklist(ctx, serverId, serverName, reason, proof_msg)
+				embed = discord.Embed(
+					color=self.bot.colors["Success"],
+					description=f'{self.bot.emojis_list["SuccessTick"]} | **{serverName.title()}** is successfully blacklisted!! ')
+				await ctx.channel.send(embed=embed)
+				# return
+			except:
+				embed = discord.Embed(
+					color=self.bot.colors["RED"],
+					description=f'{self.bot.emojis_list["Warrning"]} | Unable to blacklist them. Contact Jay or utki.')
+				await ctx.channel.send(embed=embed)
+				return
+		else:
+			try:
+				if proof_msg == "":
+					proof_msg = "N/A"
+				newvalues = {"$set": {"serverName": serverName, "reason": reason, "proof": proof_msg}}
+				self.mycol.update_one(myquery, newvalues)
+				embed = discord.Embed(
+						color=self.bot.colors["Success"],
+						description=f'{self.bot.emojis_list["SuccessTick"]} | **{serverName.title()}** is successfully updated!! ')
+				await ctx.channel.send(embed=embed)
+			except:
+				embed = discord.Embed(
+						color=self.bot.colors["RED"],
+						description=f'{self.bot.emojis_list["BrokenStatus"]} | Unable to update them. Contact Jay or utki.')
+				await ctx.channel.send(embed=embed)
+				return
 
-        if flag == 0:
-            embed = discord.Embed(
-                color=self.bot.colors["Success"],
-                description=f'{self.bot.emojis_list["SuccessTick"]} | {ctx.author.mention}, you are authorized to do partnership with them.')
-            try:
-                await ctx.author.send(embed=embed)
-            except:
-                await ctx.channel.send(embed=embed, delete_after=5)
-            return
+		embed = discord.Embed(
+					color=self.bot.colors["Success"],
+					title=f'{self.bot.emojis_list["SuccessTick"]} | **{serverName.title()}**',
+					description=f"**Server ID:** {serverId}\n"
+								f"**Server Name:** _{serverName.title()}_\n"
+								f"**Reason:** _{reason}_ \n"
+								f"**Proof:** {proof_msg} \n")
+		await ctx.channel.send(embed=embed)
 
-        dm = discord.Embed(
-            title=f"    **{dict['serverName'].title()}\n**   ",
-            description= f"**Server ID:** {dict['_id']}\n"
-                        f"**Server Name:** {dict['serverName'].title()}\n"
-                        f"**Reason:** {dict['reason']}\n",
-            color = self.bot.colors["DARK_GOLD"],
-            timestamp = datetime.datetime.utcnow()
-        )
-        dm.set_footer(text = f"Developed by utki007 & Jay", icon_url = ctx.guild.icon_url)
-        embed = discord.Embed(
-                color=self.bot.colors["RED"],
-                description=f'{self.bot.emojis_list["Warrning"]} | {ctx.author.mention}, you are **unauthorized** to do partnership with them. Contact Owners!')
-                    
-        try:
-            await ctx.author.send(embed=dm)
-        except:
-            pass
-            
-        await ctx.channel.send(embed=embed, delete_after=10)
+	@blacklist.command(name="remove", description="Remove/Delete Blacklisted Servers", aliases=['r','delete','del'])
+	@commands.check_any(checks.is_me())
+	async def delete(self, ctx, serverId: int):
+		myquery = {"_id": serverId}
+		info = self.mycol.find(myquery)
+		flag = 0
+		dict = {}
+		for x in info:
+			dict = x
+			flag = 1
+
+		if flag == 0:
+			embed = discord.Embed(
+				color=self.bot.colors["RED"],
+				description=f'{self.bot.emojis_list["Warrning"]} | Server ID not found or is no longer blacklisted.')
+			await ctx.channel.send(embed=embed)
+			return
+
+		try:
+			self.mycol.delete_one(myquery)
+			embed = discord.Embed(
+					color=self.bot.colors["Success"],
+					description=f'{self.bot.emojis_list["SuccessTick"]} | **{dict["serverName"].title()}** is successfully unblacklisted!! ')
+			await ctx.channel.send(embed=embed)
+		except:
+			embed = discord.Embed(
+					color=self.bot.colors["RED"],
+					description=f'{self.bot.emojis_list["BrokenStatus"]} | Unable to delete them.')
+			await ctx.channel.send(embed=embed)
 
 
-    @blacklist.command(name="list", description="View Blacklisted Servers", aliases=['view'])
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def list(self, ctx):
-        
-        myquery = self.mycol.find({}, {"_id": 1, "serverName": 1, "bal": 1, "reason": 1})
+	@blacklist.command(name="information", description="Check if a server is blacklisted", aliases=['info','check','status'])
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def info(self, ctx, serverId: int):
+		
+		await ctx.message.delete()
+		myquery = {"_id": serverId}
+		info = self.mycol.find(myquery)
+		flag = 0
+		dict = {}
+		for x in info:
+			dict = x
+			flag = 1
 
-        n = 0
-        list = []
-        # print the result:
-        for x in myquery:
-            dict = x
-            list.append(dict)
-        
-        for i in list:
-            try:
-                await ctx.send(
-                    f"**\n**\n**{i['serverName']}**\n"
-                    f"```ini\n"
-                    f"[ Server ID : {i['_id']}]\n"
-                    f"[ Reason : {i['reason']}]\n"
-                    f"```\n**\n**"
-                )
-            except:
-                await ctx.send(f"Unable send for {i}")
-    
+		if flag == 0:
+			embed = discord.Embed(
+				color=self.bot.colors["Success"],
+				description=f'{self.bot.emojis_list["SuccessTick"]} | {ctx.author.mention}, you are authorized to do partnership with them.')
+			try:
+				await ctx.channel.send(embed=embed)
+			except:
+				await ctx.channel.send(embed=embed, delete_after=5)
+			return
+
+		dm = discord.Embed(
+			title=f"    **{dict['serverName'].title()}\n**   ",
+			description= f"**Server ID:** {dict['_id']}\n"
+						f"**Server Name:** {dict['serverName'].title()}\n"
+						f"**Reason:** {dict['reason']}\n"
+						f"**Proof:** {dict['proof']} \n",
+			color = self.bot.colors["DARK_GOLD"],
+			timestamp = datetime.datetime.utcnow()
+		)
+		dm.set_footer(text = f"Developed by utki007 & Jay", icon_url = ctx.guild.icon_url)
+		embed = discord.Embed(
+				color=self.bot.colors["RED"],
+				description=f'{self.bot.emojis_list["Warrning"]} | {ctx.author.mention}, you are **unauthorized** to do partnership with them. Contact Owners!')
+					
+		try:
+			await ctx.author.send(embed=dm)
+		except:
+			pass
+			
+		await ctx.channel.send(f'{ctx.author.mention}, Check your dms', delete_after=5)
+
+
+	@blacklist.command(name="list", description="View Blacklisted Servers", aliases=['view'])
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def list(self, ctx):
+		
+		myquery = self.mycol.find({}, {"_id": 1, "serverName": 1, "bal": 1, "reason": 1, "proof": 1})
+
+		n = 0
+		list = []
+		# print the result:
+		for x in myquery:
+			dict = x
+			list.append(dict)
+		
+		for i in list:
+			try:
+				embed = discord.Embed(
+					color=self.bot.colors["Success"],
+					title=f'{self.bot.emojis_list["SuccessTick"]} | **{i["serverName"].title()}**',
+					description=f"**Server ID:** {i['_id']}\n"
+								f"**Server Name:** _{i['serverName']}_\n"
+								f"**Reason:** _{i['reason']}_ \n"
+								f"**Proof:** {i['proof']} \n")
+				embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/859665223388233748.gif?size=128&quality=lossless")
+				await ctx.channel.send(embed=embed)
+
+				# await ctx.send(
+				#     f"**\n**\n**{i['serverName']}**\n"
+				#     f"```ini\n"
+				#     f"[ Server ID : {i['_id']}]\n"
+				#     f"[ Reason : {i['reason']}]\n"
+				#     f"```\n**\n**"
+				# )
+			except:
+				await ctx.send(f"Unable to send for {i}")
+	
 
 def setup(bot):
    bot.add_cog(blacklist(bot))
--- cogs/serverutils.py
+++ cogs/serverutils.py
@@ -1,144 +1,161 @@
 # importing the required libraries
 import discord
 from discord.ext import commands, tasks
-import os
 import pandas as pd
 import numpy as np
-import pymongo
-import dns
 import time 
-import asyncio
-import math
 import datetime
-import shlex
-import colour
 from colour import Color
-import shlex
-import random 
 from utils.Checks import checks
 class serverutils(commands.Cog, description="Server Utility"):
-    
-    def __init__(self, bot):
-        self.bot= bot
+	
+	def __init__(self, bot):
+		self.bot= bot
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
-    
-    @commands.command(no_pm=True)
-    #@commands.check_any(commands.has_any_role(785842380565774368 ,799037944735727636,785845265118265376,787259553225637889,843775369470672916), commands.is_owner())
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def poll(self, ctx, *, question: str):
-        """
-        Quick and easy yes/no poll, for multiple answers, see !quickpoll
-        """
-        msg = await ctx.send("**{}** asks: {}?".format(ctx.message.author, question.replace("@", "@\u200b")))
-        try:
-            await ctx.message.delete()
-        except:
-            pass
-        playzone = self.bot.get_guild(815849745327194153)
-        
-        yes = await playzone.fetch_emoji(942341153573978113)
-        no = await playzone.fetch_emoji(942341223576920064)
-        await msg.add_reaction(yes)
-        await msg.add_reaction(no)
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+	
+	@commands.command(no_pm=True)
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def poll(self, ctx, *, question: str):
+		"""
+		Quick and easy yes/no poll, for multiple answers, see !quickpoll
+		"""
+		msg = await ctx.send("**{}** asks: {}?".format(ctx.message.author, question.replace("@", "@\u200b")))
+		try:
+			await ctx.message.delete()
+		except:
+			pass
+		playzone = self.bot.get_guild(815849745327194153)
+		
+		yes = await playzone.fetch_emoji(942341153573978113)
+		no = await playzone.fetch_emoji(942341223576920064)
+		await msg.add_reaction(yes)
+		await msg.add_reaction(no)
   
-    @commands.command(name="colour",aliases=["co","col"])
-    #@commands.check_any(commands.has_any_role(785842380565774368 ,799037944735727636,785845265118265376,787259553225637889,843775369470672916), commands.is_owner())
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def colour(self,ctx,color:discord.Color):
-        await ctx.message.delete()
-        name = Color(str(color))
-        url = f"https://serux.pro/rendercolour?hex={str(color)[1:]}"
-        color = discord.Embed(
-            title=f" {str(name).upper()} ",
-            description=f"**RGB:** {color.to_rgb()} \n**COLOUR:** {color}",
-            color= color,
-            timestamp=datetime.datetime.utcnow()
-        )
-        color.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
-        color.set_thumbnail(url=url)
-        color.set_footer(
-                text=f"Developed by utki007 & Jay")
-        await ctx.send(embed=color)
+	@commands.command(name="colour",aliases=["co","col"])
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def colour(self,ctx,color:discord.Color):
+		await ctx.message.delete()
+		name = Color(str(color))
+		url = f"https://serux.pro/rendercolour?hex={str(color)[1:]}"
+		color = discord.Embed(
+			title=f" {str(name).upper()} ",
+			description=f"**RGB:** {color.to_rgb()} \n**COLOUR:** {color}",
+			color= color,
+			timestamp=datetime.datetime.utcnow()
+		)
+		color.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
+		color.set_thumbnail(url=url)
+		color.set_footer(
+				text=f"Developed by utki007 & Jay")
+		await ctx.send(embed=color)
   
-    
-    # guild = self.bot.get_guild(785839283847954433)
-        # members = guild.members
-    @commands.command(name="gamer")
-    #@commands.check_any(commands.has_any_role(785842380565774368 ,799037944735727636), commands.is_owner())
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def gamer(self,ctx):
-        
-        start = time.time()
-        
-        guild = self.bot.get_guild(785839283847954433)
-        users = guild.members
-        
-        gamer = discord.utils.get(guild.roles, id=790667905330970674)
-        bgmi = discord.utils.get(guild.roles, id=795711140108697630)
-        trainee = discord.utils.get(guild.roles, id=811307500321505320)
-        bl = discord.utils.get(guild.roles, id=936900151325360138)
-        valorant = discord.utils.get(guild.roles, id=795711130378829835)
-        
-        am = discord.AllowedMentions(
-            users=False,  # Whether to ping individual user @mentions
-            everyone=False,  # Whether to ping @everyone or @here mentions
-            roles=False,  # Whether to ping role @mentions
-            replied_user=False,  # Whether to ping on replies to messages
-        )
-        await ctx.message.delete()
-        message = await ctx.send(f"Starting to assign gamer role to members ... ", allowed_mentions=am)
-        j = 0
-        for i in users:
-            if (bgmi in i.roles or valorant in i.roles) and (trainee in i.roles and gamer not in i.roles and bl not in i.roles):
-                try:
-                    await i.add_roles(gamer)
-                    await ctx.send(f"{i.name} has been given {gamer.mention} role.", allowed_mentions=am)
-                    j = j + 1
-                except:
-                    await ctx.send(f"Failed to assign {gamer.mention} role to {i.mention}", allowed_mentions=am)
-        
-        end = time.time()
-        if j != 0:    
-            await ctx.send(f"Took {round((end - start) * 1000, 3)} ms to assign role to {j} members")
-        else:
-            await message.edit(content=f"No user is left to be assigned {gamer.mention} role!", allowed_mentions=am)
-    
-    @commands.command(name="bar",description="To be used in public channels after completing a task")
-    #@commands.check_any(commands.has_any_role(785842380565774368 ,799037944735727636,785845265118265376,787259553225637889,843775369470672916), commands.is_owner())
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def bar(self,ctx):
-        await ctx.message.delete()
-        l = ["https://cdn.discordapp.com/attachments/782701143222386718/809423966862311424/1JOZT-rbar.gif"]
-        # await ctx.send(random.choice(l))
-        await ctx.send(l[0])
+	
+	@commands.command(name="gamer")
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def gamer(self,ctx):
+		
+		start = time.time()
+		
+		guild = self.bot.get_guild(785839283847954433)
+		users = guild.members
+		
+		gamer = discord.utils.get(guild.roles, id=790667905330970674)
+		bgmi = discord.utils.get(guild.roles, id=795711140108697630)
+		trainee = discord.utils.get(guild.roles, id=811307500321505320)
+		bl = discord.utils.get(guild.roles, id=936900151325360138)
+		valorant = discord.utils.get(guild.roles, id=795711130378829835)
+		
+		am = discord.AllowedMentions(
+			users=False,  # Whether to ping individual user @mentions
+			everyone=False,  # Whether to ping @everyone or @here mentions
+			roles=False,  # Whether to ping role @mentions
+			replied_user=False,  # Whether to ping on replies to messages
+		)
+		await ctx.message.delete()
+		message = await ctx.send(f"Starting to assign gamer role to members ... ", allowed_mentions=am)
+		j = 0
+		for i in users:
+			if (bgmi in i.roles or valorant in i.roles) and (trainee in i.roles and gamer not in i.roles and bl not in i.roles):
+				try:
+					await i.add_roles(gamer)
+					await ctx.send(f"{i.name} has been given {gamer.mention} role.", allowed_mentions=am)
+					j = j + 1
+				except:
+					await ctx.send(f"Failed to assign {gamer.mention} role to {i.mention}", allowed_mentions=am)
+		
+		end = time.time()
+		if j != 0:    
+			await ctx.send(f"Took {round((end - start) * 1000, 3)} ms to assign role to {j} members")
+		else:
+			await message.edit(content=f"No user is left to be assigned {gamer.mention} role!", allowed_mentions=am)
+	
+	@commands.command(name="bar",description="To be used in public channels after completing a task")
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def bar(self,ctx):
+		await ctx.message.delete()
+		l = ["https://cdn.discordapp.com/attachments/782701143222386718/809423966862311424/1JOZT-rbar.gif"]
+		# await ctx.send(random.choice(l))
+		await ctx.send(l[0])
 
-    @commands.command(name="dm",description="Dm a user!")
-    #@commands.check_any(commands.has_any_role(785842380565774368 ,799037944735727636,785845265118265376,787259553225637889,843775369470672916), commands.is_owner())
-    @commands.check_any(checks.can_use(), checks.is_me())
-    async def dm(self,ctx,member: discord.Member,*,message: str):
-        await ctx.message.delete()
-        l = ["https://cdn.discordapp.com/attachments/782701143222386718/809423966862311424/1JOZT-rbar.gif"]
-        # await ctx.send(random.choice(l))
-        await ctx.send(f"Want to dm {member} with message: {message} \n`(Yes/No)`")
-        try:
-            msg = await self.bot.wait_for("message", check=lambda m: m.author.id == ctx.author.id and m.content.lower() in ["yes","y","no","n"], timeout=10)
-            
-            if msg.content.lower() in ["y","yes"]:
-                try:
-                    await member.send(message)
-                    await ctx.channel.send(f"Dm'ed {member} with message: {message}")
-                except:
-                    await ctx.channel.send(f"Unable to dm {member} with message: {message}")
-            else:
-                await ctx.channel.send(f"{ctx.author.mention}, DM cancelled")
-        except:
-            await ctx.send(f"{ctx.author.mention}, DM was not sent because there was no confirmation!")
-        
-            
-        
-      
+	@commands.command(name="dm",description="Dm a user!")
+	@commands.check_any(checks.can_use(), checks.is_me())
+	async def dm(self,ctx,member: discord.Member,*,message: str):
+		await ctx.message.delete()
+		l = ["https://cdn.discordapp.com/attachments/782701143222386718/809423966862311424/1JOZT-rbar.gif"]
+		# await ctx.send(random.choice(l))
+		await ctx.send(f"Want to dm {member} with message: {message} \n`(Yes/No)`")
+		try:
+			msg = await self.bot.wait_for("message", check=lambda m: m.author.id == ctx.author.id and m.content.lower() in ["yes","y","no","n"], timeout=10)
+			
+			if msg.content.lower() in ["y","yes"]:
+				try:
+					await member.send(message)
+					await ctx.channel.send(f"Dm'ed {member} with message: {message}")
+				except:
+					await ctx.channel.send(f"Unable to dm {member} with message: {message}")
+			else:
+				await ctx.channel.send(f"{ctx.author.mention}, DM cancelled")
+		except:
+			await ctx.send(f"{ctx.author.mention}, DM was not sent because there was no confirmation!")
+
+	# @commands.command(name="star",description="To star a message")
+	# @commands.check_any(checks.can_use(), checks.is_me())
+	# async def star(self,ctx):
+	# 	message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+	# 	embeds = message.embeds
+	# 	dict = {}
+	# 	for embed in embeds:
+	# 		dict = embed.to_dict()
+	# 	gk = self.bot.get_guild(785839283847954433)
+	# 	starboard = gk.get_channel(930922016255656026)
+		
+	# 	# footer = {}
+	# 	# footer["text"] = f"Developed by utki007 & Jay"
+	# 	# footer["icon_url"] = ctx.guild.icon_url
+	# 	# dict["footer"] = footer
+	# 	dict["footer"] = {'text':f"Developed by utki007 & Jay", 'icon_url': f'{ctx.guild.icon_url}'}
+	# 	field = {'name': "Source", 'value': f"[Jump!]({message.jump_url})", 'inline': False}
+		
+	# 	if 'fields' not in dict:
+	# 		dict["fields"]= []
+	# 	dict['fields'].append(field)
+
+	# 	# if 'timestamp' not in dict:
+	# 	# 	dict["timestamp"] = f'{int(datetime.datetime.now().timestamp())}'
+	# 	# else:
+	# 	# 	timestamp = str(dict['timestamp'].split('+')[0])
+	# 	# 	dict['timestamp'] = f'<t:{int(datetime.strptime(timestamp,"%Y-%m-%dT%H:%M:%S.%f").timestamp())}:D>'
+
+	# 	# await ctx.send(dict['timestamp'])
+	# 	# await ctx.send(message)
+	# 	await ctx.send(content=message.content,embed = discord.Embed.from_dict(dict))
+
+	# 	# message = await starboard.send(content=message.content,embed = embed.from_dict(dict))
+			
+		
+	  
 def setup(bot):
-    bot.add_cog(serverutils(bot))
+	bot.add_cog(serverutils(bot))
