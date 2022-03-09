de46b13884fd38c317868b183641fc1080317169
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-03-09 23:31:34 +0530

    Update channel.py

--- cogs/channel.py
+++ cogs/channel.py
@@ -424,39 +424,23 @@ class channel(commands.Cog, description="Channel utils"):
         await channel.send(embed=embed)
    
     @cog_ext.cog_subcommand(base="Channel", name="Add",description="Add a role woth view + type perms", guild_ids=[785839283847954433],
-		base_default_permission=False, base_permissions=founder_perm,
-        options=[
-            create_option(name="role", description="Enter role to add to channel", required=False, option_type=8),
-            create_option(name="state", description="Enter Stats of lock", required=False, option_type=5)
-        ]
+		base_default_permission=False, base_permissions=founder_perm
     )
-    async def channeladd(self, ctx, state: bool = True, role: discord.Role = None):
+    async def channeladd(self, ctx,  role: discord.Role):
 
-        channel = ctx.channel        
-        if role == None:
-            role = discord.utils.get(ctx.guild.roles, id=self.default_role)
+        channel = ctx.channel    
+        secret = discord.utils.get(ctx.guild.roles, id=941772431750750218)
+        admin = discord.utils.get(ctx.guild.roles, id=785845265118265376)
 
         overwrite = channel.overwrites_for(role)
         
-        if state == True:
-            overwrite.send_messages = True
-            overwrite.view_channel = True
-        elif state == False:
-            overwrite.send_messages = None
-            overwrite.view_channel = None
 
-        msg = ''
-        
-        if state:
-            msg = f':white_check_mark: | Added {role.mention} to **{channel}** with state `True`'
-        else:
-            msg = f':white_check_mark: | Added {role.mention} to **{channel}**'
-        
-        await ctx.send(f"Added {role.mention} to **{channel}** with state `{state}`", hidden=True)
-        await channel.set_permissions(role, overwrite=overwrite)
+        overwrite.send_messages = True
+        overwrite.view_channel = True
+        await channel.set_permissions(secret, overwrite=overwrite)
+        await channel.set_permissions(admin, overwrite=overwrite)
+        await ctx.send(f"Added {admin.mention} and {secret.mention} to **{channel}** ", hidden=True)
 
-        embed = discord.Embed(
-            color=0x78AB46, description=f'{msg}')
 
 def setup(bot):
    bot.add_cog(channel(bot))
