3588c2ae02e40e4afa7d1bf63119d427fde9cece
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-07-17 17:59:21 +0530

    Removing extra white spaces

--- main.py
+++ main.py
@@ -260,7 +260,6 @@ bot.number_emojis = {
 	"9": "<:tgk_nine:997925086281613426>"
 }
 
-
 if __name__ == "__main__":
 	bot.mongo = motor.motor_asyncio.AsyncIOMotorClient(str(bot.connection_url))
 	bot.db = bot.mongo["TGK"]
