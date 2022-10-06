5ad63315a2b9ee181876c8fc7d84d89e06e40528
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-10-06 14:27:26 +0530

    Update custom_pagination.py

--- utils/custom_pagination.py
+++ utils/custom_pagination.py
@@ -20,10 +20,16 @@ async def addPages(client,ctx,message,pages):
             if i > 0:
                 i -= 1
                 await message.edit(embed = pages[i])
+            else:
+                await message.edit(embed = pages[0])
+                i = 0
         elif str(reaction) == client.emojis_list["right"]:
             if i < len(pages):
                 i += 1
                 await message.edit(embed = pages[i])
+            else:
+                await message.edit(embed = pages[len(pages)-1])
+                i = len(pages)-1
         elif  str(reaction) == client.emojis_list["rightArrow"]:
             await message.edit(embed = pages[len(pages)-1])
             i = len(pages)-1
