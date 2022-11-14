078d34531041dd473c85d16b80b77a3e6e6b401e
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-11-14 22:30:26 +0530

    Update heistroles.py

--- cogs/heistroles.py
+++ cogs/heistroles.py
@@ -54,7 +54,7 @@ class heistroles(commands.Cog):
 		if self.bot.user.id == 859107514082394142:
 			return
 
-		if message.author.id == 646937666251915264 and message.content != None and "server is currenty" in message.content:
+		if message.author.id == 646937666251915264 and message.content != None and "server is currently" in message.content:
 			await message.channel.send(f"> <@&1034072149247397938> form up for _server drop_!")  
 
 		if message.author.id == 270904126974590976:
