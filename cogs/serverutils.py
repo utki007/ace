b1f95916c80a132188941a7c5b979db556df5067
Jay Patel <pateljay24004@gmail.com>
2023-10-26 08:01:05 +0000

    updated bar cmd

--- cogs/serverutils.py
+++ cogs/serverutils.py
@@ -113,7 +113,8 @@ class serverutils(commands.Cog, description="Server Utility"):
 		gif_types = {
 			"default": "https://cdn.discordapp.com/attachments/810050662686523394/1061588592864010310/tgk_black_bar.gif",
 			"snow": "https://media.discordapp.net/attachments/840291742859001876/943806099537162250/0E67BE40-2287-4A6F-9520-C6FD5E548227.gif",
-			"halloween": "https://cdn.discordapp.com/attachments/1144670888797667469/1166941685213827134/IMG_5389.gif?ex=654c5222&is=6539dd22&hm=4078f851e50d96a8e3a1e4110b6a3cf49abcf5e92367ffbbaae93db5715f6a86&"
+			"halloween": "https://cdn.discordapp.com/attachments/1144670888797667469/1166941685213827134/IMG_5389.gif?ex=654c5222&is=6539dd22&hm=4078f851e50d96a8e3a1e4110b6a3cf49abcf5e92367ffbbaae93db5715f6a86&",
+			"halloween2": "https://68.media.tumblr.com/2c5ac33b9a0a8a680dc4374fcba6b071/tumblr_nsbo2nPupG1tl8p9uo1_500.gif"
 		}
 		try: 
 			gif = gif_types[type]
